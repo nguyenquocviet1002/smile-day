f1e61f10974a301fd2f4bb59aa4099812d9fbee9
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2025-03-19 17:42:59 +0700

    update

--- js/script.js
+++ js/script.js
@@ -61,6 +61,70 @@ const funcDragDoctor_knhd_1_0_0 = (element) => {
 
 funcDragDoctor_knhd_1_0_0('.screen4__list');
 
+const addError = (elm) => {
+  elm.parentNode.classList.add('error');
+  elm.parentNode.querySelector('.form__message').innerText = 'Vui lòng điền thông tin';
+}
+
+const removeError = (elm) => {
+  elm.parentNode.classList.remove('error');
+  elm.parentNode.querySelector('.form__message').innerText = '';
+}
+
+const firstForm = () => {
+  const name = document.querySelector('.screen10__form input[name="name"]');
+  const phone = document.querySelector('.screen10__form input[name="phone"]');
+  const submit = document.querySelector('.screen10__form input[type="button"]');
+  submit.addEventListener('click', () => {
+    removeError(name);
+    removeError(phone);
+    if (!name.value || !phone.value) {
+      if (!name.value) {
+        addError(name);
+      }
+      if (!phone.value) {
+        addError(phone);
+      }
+    } else {
+      submit.setAttribute('disabled', '');
+      submit.value = 'Đang gửi...';
+      setTimeout(() => {
+        submit.removeAttribute('disabled');
+        submit.value = 'Đặt hẹn ngay';
+        alert("Thành công!");
+      }, 3000)
+      $.ajax({
+        url: "https://docs.google.com/forms/d/e/1FAIpQLScD_eN89d9wIrOXPJk3fyOM5841-B5AicupcABWVHjHayE2Nw/formResponse",
+        data: {
+          "entry.1745789035": name.value,
+          "entry.886269586": phone.value,
+        },
+        type: "POST",
+        dataType: "json",
+        statusCode: {
+          0: function (data) {
+          },
+          200: function (data) {
+          },
+        },
+      });
+      name.value = '';
+      phone.value = '';
+    }
+
+  })
+}
+firstForm();
+
+AOS.init();
+
+const items9 = document.querySelectorAll('.screen9__item');
+items9.forEach(item => {
+  item.addEventListener('click', () => {
+    const id = item.getAttribute('data-video');
+  })
+})
+
 // document.querySelector('.screen4__btn').addEventListener('click', () => {
 //   document.querySelector('#modal-pop').style.display = 'flex';
 // });
